554d710e048bc321c756ac47a6c86dd6104429b8
Frenzla <86185774+frenzla@users.noreply.github.com>
2024-07-17 21:26:39 +0200

    args checking done

--- srcs/client/client.c
+++ srcs/client/client.c
@@ -6,7 +6,7 @@
 /*   By: alarose <alarose@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/16 17:09:21 by alarose           #+#    #+#             */
-/*   Updated: 2024/07/16 21:47:21 by alarose          ###   ########.fr       */
+/*   Updated: 2024/07/17 18:41:09 by alarose          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,29 +26,41 @@ int ft_is_number(char *arg)
 	return (1);
 }
 
-int	check_args(int argc, char **argv, pid_t *pid, char *str)
+char	*check_args(int argc, char **argv, pid_t *pid)
 {
-	(void) str; // to check str
+	char *str;
+
+	str = NULL;
 	if (argc != 3)
-		return (ft_printf(RED "Error: Wrong nb of args\n" RESET), RET_ERR);
+		return (ft_printf(RED "Error: Wrong nb of args\n" RESET), NULL);
 	if (ft_is_number(argv[1]) && kill(ft_atoi(argv[1]), 0) == 0)
+	{
 		*pid = ft_atoi(argv[1]);
+		str = ft_strdup(argv[2]);
+	}
 	else if (ft_is_number(argv[2]) && kill(ft_atoi(argv[2]), 0) == 0)
+	{
 		*pid = ft_atoi(argv[2]);
+		str = ft_strdup(argv[1]);
+	}
 	else
-		return(ft_printf(RED "Error: wrong PID or process not active\n" RESET), RET_ERR);
-	return (1);
+		return(ft_printf(RED "Error: wrong PID or process not active\n" RESET), NULL);
+	if (!str)
+		return(ft_printf(RED "Error: Give a msg to print\n" RESET), NULL);
+	return (str);
 }
 
 int	main(int argc, char **argv)
 {
 	pid_t	pid;
 	char	*str;
+//	size_t	len;
 
 	str = NULL;
 	pid = -1;
-	if (!check_args(argc, argv, &pid, str))
-		return (1);
+	str = check_args(argc, argv, &pid);
 	ft_printf("Valid PID: %d\n", pid);
+	ft_printf("str      : %s\n", str);
+
 	return (0);
 }
